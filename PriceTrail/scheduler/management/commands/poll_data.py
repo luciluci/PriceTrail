95d102781d73103f45ebd804d647f999324026c6
luciluci <lucian.apetre@gmail.com>
2018-01-28 21:44:22 +0200

    Add command to append product prices

---
+++ PriceTrail/scheduler/management/commands/poll_data.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand
+
+from TailedProducts.models import Product, ProductPrice
+from PriceTrail.spiders import GiantSpiders
+from PriceTrail.utils import data
+
+import httplib
+
+class Command(BaseCommand):
+    help = 'Add price entry for all products'
+
+    def handle(self, *args, **options):
+        help = "Append price each day for each product"
+
+        monitored_products = Product.objects.all()
+        for product in monitored_products:
+            shop = product.shop
+
+            if shop == 'emag':
+                spider = GiantSpiders.EmagSpider()
+                response = spider.req_product(product.url)
+                if httplib.OK == response:
+                    prod = spider.get_product()
+                    price = prod.price
+
+                    new_prod_price = ProductPrice()
+                    new_prod_price.price = price
+                    new_prod_price.product = product
+                    new_prod_price.save()
+                    self.stdout.write(self.style.SUCCESS(prod.name + ' - OK'))
+                elif data.PRODUCT_UNAVAILABLE == response:
+                    self.stdout.write(self.style.SKIP(prod.name + ' - UNAVAILABLE'))
+                else:
+                    self.stdout.write(self.style.ERROR(prod.name + ' - NOK'))
+            else:
+                self.stdout.write(self.style.ERROR('SHOP NOT SUPPORTED: ' + shop))
