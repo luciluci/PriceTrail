97d4e93a0148e03b841c502eabb63814ffd91286
luciluci <lucian.apetre@gmail.com>
2018-04-28 22:27:06 +0300

    Added affiliates for germanos

--- PriceTrail/PriceTrail/utils/affiliates.py
+++ PriceTrail/PriceTrail/utils/affiliates.py
@@ -1,6 +1,7 @@
 
 EVOMAG_AFFILIATE_STR = 'http://event.2performant.com/events/click?ad_type=quicklink&aff_code=b42f1f3d6&unique=d4f678b43&redirect_to='
 CEL_AFFILIATE_URL = 'http://event.2performant.com/events/click?ad_type=quicklink&aff_code=b42f1f3d6&unique=468ff75f3&redirect_to='
+GERMANOS_AFFILIATE_URL = 'http://event.2performant.com/events/click?ad_type=quicklink&aff_code=b42f1f3d6&unique=63ed62863&redirect_to='
 
 class Affiliate:
 
@@ -10,5 +11,7 @@ class Affiliate:
             return EVOMAG_AFFILIATE_STR + url
         elif shop == 'cel':
             return CEL_AFFILIATE_URL + url
+        elif shop == 'germanos':
+            return GERMANOS_AFFILIATE_URL + url
         else:
             return url
